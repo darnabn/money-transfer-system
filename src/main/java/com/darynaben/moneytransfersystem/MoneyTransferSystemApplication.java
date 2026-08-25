b70911bdbf45a5ad241fc7d996651ecb87e5db7d
Daryn <darynaben.dev@gmail.com>
2026-08-25 15:30:24 +0500

    Initial project setup with Spring Boot

---
+++ src/main/java/com/darynaben/moneytransfersystem/MoneyTransferSystemApplication.java
@@ -0,0 +1,13 @@
+package com.darynaben.moneytransfersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyTransferSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyTransferSystemApplication.class, args);
+    }
+
+}
